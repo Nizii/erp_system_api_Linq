d10adc759fde4c8c69c9d7dea300a5fa40eee025
Nizam <nizam.oezdemir@gmx.ch>
2022-10-20 11:34:59 +0200

    Product added

--- WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/Controllers/ProductController.cs
@@ -2,9 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
-using System;
 using System.Linq;
-using System.Threading.Tasks;
 using WebApplication1.Models;
 using System.Diagnostics;
 
@@ -12,12 +10,12 @@ namespace WebApplication1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CustomerController : ControllerBase
+    public class ProductController : ControllerBase
     {
 
         private readonly IConfiguration _configuration;
         private readonly IWebHostEnvironment _env;
-        public CustomerController(IConfiguration configuration, IWebHostEnvironment env)
+        public ProductController(IConfiguration configuration, IWebHostEnvironment env)
         {
             _configuration = configuration;
             _env = env;
@@ -28,7 +26,7 @@ namespace WebApplication1.Controllers
         public JsonResult Get()
         {
             MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
-            var dbList = dbClient.GetDatabase("Database").GetCollection<Customer>("Customer").AsQueryable();
+            var dbList = dbClient.GetDatabase("Database").GetCollection<Product>("Product").AsQueryable();
             return new JsonResult(dbList);
         }
 
@@ -36,39 +34,39 @@ namespace WebApplication1.Controllers
         public JsonResult Get(int id)
         {
             MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
-            var dbList = dbClient.GetDatabase("Database").GetCollection<Customer>("Customer").AsQueryable();
-            Customer customer = null;
-            foreach (var customer_from_db in dbList)
+            var dbList = dbClient.GetDatabase("Database").GetCollection<Product>("Product").AsQueryable();
+            Product product = null;
+            foreach (var product_from_db in dbList)
             {
-                if (id.Equals(customer_from_db.customer_nr))
+                if (id.Equals(product_from_db.product_nr))
                 {
-                    customer = customer_from_db;
+                    product = product_from_db;
                     break;
                 }
             }
-            return new JsonResult(customer);
+            return new JsonResult(product);
         }
 
 
         [HttpPost]
-        public JsonResult Post(Customer cus)
+        public JsonResult Post(Product pro)
         {
             MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
-            int LastCustomerId = dbClient.GetDatabase("Database").GetCollection<Customer>("Customer").AsQueryable().Count();
-            cus.customer_nr = LastCustomerId + 1;
-            dbClient.GetDatabase("Database").GetCollection<Customer>("Customer").InsertOne(cus);
+            int LastProductId = dbClient.GetDatabase("Database").GetCollection<Product>("Product").AsQueryable().Count();
+            pro.product_nr = LastProductId + 1;
+            dbClient.GetDatabase("Database").GetCollection<Product>("Product").InsertOne(pro);
             return new JsonResult("Added Successfully");
         }
 
        
         [HttpPut]
-        public JsonResult Put(Customer cus)
+        public JsonResult Put(Product pro)
         {
  
             MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
-            var filter = Builders<Customer>.Filter.Eq("customer_nr", cus.customer_nr);
+            var filter = Builders<Product>.Filter.Eq("product_nr", pro.product_nr);
             Debug.WriteLine("Filter "+filter);
-
+            /*
             Debug.WriteLine("Customer_nr " + cus.customer_nr);
             Debug.WriteLine("Debug " + cus.surname);
             Debug.WriteLine("Debug " + cus.lastname);
@@ -80,22 +78,18 @@ namespace WebApplication1.Controllers
             Debug.WriteLine("Debug " + cus.landlinephone);
             Debug.WriteLine("Debug " + cus.note);
             Debug.WriteLine("Debug " + cus.email);
-
-            var update = Builders<Customer>.Update.Set("surname", cus.surname)
-                                                    .Set("lastname", cus.lastname)
-                                                    .Set("dob", cus.dob)
-                                                    .Set("street", cus.street)
-                                                    .Set("nr", cus.nr)
-                                                    .Set("postcode", cus.postcode)
-                                                    .Set("country", cus.country)
-                                                    .Set("cellphone", cus.cellphone)
-                                                    .Set("landlinephone", cus.landlinephone)
-                                                    .Set("note", cus.note)
-                                                    .Set("email", cus.email);
+            */
+            var update = Builders<Product>.Update.Set("product_name", pro.product_name)
+                                                    .Set("product_size", pro.product_size)
+                                                    .Set("description", pro.description)
+                                                    .Set("units_available", pro.units_available)
+                                                    .Set("unit", pro.unit)
+                                                    .Set("purchasing_price_per_unit", pro.purchasing_price_per_unit)
+                                                    .Set("selling_price_per_unit", pro.selling_price_per_unit)
             Debug.WriteLine("Update " + update);
-            dbClient.GetDatabase("Database").GetCollection<Customer>("Customer").UpdateOne(filter, update);
+            dbClient.GetDatabase("Database").GetCollection<Product>("Product").UpdateOne(filter, update);
             //return new JsonResult("Updated Successfully");
-            return new JsonResult(cus);
+            return new JsonResult(pro);
         }
         
 
@@ -104,9 +98,9 @@ namespace WebApplication1.Controllers
         public JsonResult Delete(int id)
         {
             MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
-            var filter = Builders<Customer>.Filter.Eq("customer_nr", id);
-            dbClient.GetDatabase("Database").GetCollection<Customer>("Customer").DeleteOne(filter);
-            return new JsonResult("Kunde wurde erfolgreich gelöscht");
+            var filter = Builders<Product>.Filter.Eq("product_nr", id);
+            dbClient.GetDatabase("Database").GetCollection<Product>("Product").DeleteOne(filter);
+            return new JsonResult("Produkt wurde erfolgreich gelöscht");
         }
     }
 }
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using System;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public ObjectId Id { get; set; }
+
+        public Int32 product_nr { get; set; }
+
+        public String product_name { get; set; }
+
+        public Int32 product_size { get; set; }
+
+        public String description { get; set; }
+
+        public String units_available { get; set; }
+
+        public String unit { get; set; }
+
+        public Int32 purchasing_price_per_unit { get; set; }
+
+        public Int32 selling_price_per_unit { get; set; }
+    }
+}
