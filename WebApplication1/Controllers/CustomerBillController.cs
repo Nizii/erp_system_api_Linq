f0e1b0f38f8827b09f14d50fcfa43d88c583e690
Nizam <nizam.oezdemir@gmx.ch>
2022-10-20 13:50:15 +0200

    CustomerBill implemented

---
+++ WebApplication1/Controllers/CustomerBillController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerBillController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public CustomerBillController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
+
+            var dbList = dbClient.GetDatabase("Database").GetCollection<CustomerBill>("CustomerBill").AsQueryable();
+
+            return new JsonResult(dbList);
+        }
+
+        [HttpPost]
+        public JsonResult Post(CustomerBill bill)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
+
+            int LastDepartmentId = dbClient.GetDatabase("Database").GetCollection<CustomerBill>("CustomerBill").AsQueryable().Count();
+            bill.customer_bill_nr = LastDepartmentId + 1;
+
+            dbClient.GetDatabase("Database").GetCollection<CustomerBill>("CustomerBill").InsertOne(bill);
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(CustomerBill bill)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
+            var filter = Builders<CustomerBill>.Filter.Eq("customer_bill_nr", bill.customer_bill_nr);
+
+            /*
+            Debug.WriteLine("Customer_nr " + cus.customer_nr);
+            Debug.WriteLine("Debug " + cus.surname);
+            Debug.WriteLine("Debug " + cus.lastname);
+            Debug.WriteLine("Debug " + cus.dob);
+            Debug.WriteLine("Debug " + cus.street);
+            Debug.WriteLine("Debug " + cus.postcode);
+            Debug.WriteLine("Debug " + cus.country);
+            Debug.WriteLine("Debug " + cus.cellphone);
+            Debug.WriteLine("Debug " + cus.landlinephone);
+            Debug.WriteLine("Debug " + cus.note);
+            Debug.WriteLine("Debug " + cus.email);
+            
+            var update = Builders<Product>.Update.Set("product_name", pro.product_name)
+                                                    .Set("product_size", pro.product_size)
+                                                    .Set("description", pro.description)
+                                                    .Set("units_available", pro.units_available)
+                                                    .Set("unit", pro.unit)
+                                                    .Set("purchasing_price_per_unit", pro.purchasing_price_per_unit)
+                                                    .Set("selling_price_per_unit", pro.selling_price_per_unit);
+            dbClient.GetDatabase("Database").GetCollection<CustomerBill>("CustomerBill").UpdateOne(filter, update);
+            */
+            //return new JsonResult("Updated Successfully");
+            return new JsonResult(bill);
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("ConnectionStringForDatabase"));
+
+            var filter = Builders<CustomerBill>.Filter.Eq("customer_bill_nr", id);
+
+
+            dbClient.GetDatabase("Database").GetCollection<CustomerBill>("CustomerBill").DeleteOne(filter);
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+
+    }
+}
---
+++ WebApplication1/Models/CustomerBill.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CustomerBill
+    {
+        public ObjectId Id { get; set; }
+
+        public String company_name { get; set; }
+        public String customer_name { get; set; }
+
+        public String customer_street { get; set; }
+
+        public String customer_postcode { get; set; }
+
+        public Int32 amount { get; set; }
+
+        public String currency { get; set; }
+
+        public String issued_on { get; set; }
+
+        public String deadline { get; set; }
+
+        public Int32 customer_bill_nr { get; set; }
+    }
+}
