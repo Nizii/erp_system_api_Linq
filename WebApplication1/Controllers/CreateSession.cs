596fca4bbfbdb09776feff112608b75864030f2d
Nizam <nizam.oezdemir@gmx.ch>
2022-11-21 12:57:47 +0100

    Post method impl

--- WebApplication1/Controllers/CreateSession.cs
+++ WebApplication1/Controllers/CreateSession.cs
@@ -1,19 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using System.Linq;
-using Microsoft.Extensions.Caching.Memory;
-using System.Collections.Generic;
 using System.Data;
 using Microsoft.AspNetCore.Http;
 using ErpSystemDbContext;
 using User = ErpSystemDbContext.User;
-using WebApplication1;
-using System.Diagnostics;
 
 namespace WebApplication1
 {
-
     [Route("api")]
     public class TestController : Controller
     {
@@ -23,20 +17,27 @@ namespace WebApplication1
         [Route("{username}/{password}")]
         public JsonResult Get(string username, string password)
         {
-            Debug.WriteLine("Get Request has been triggered");
-            User user = GetUser(username, password);
-            Debug.Write("Login "+user.UserName);
+            User user = GetUserFromDB(username, password);
             if (user != null)
             {
                 HttpContext.Session.Set<User>(username, user);
-                Debug.Write("Login Succeed");
-                return new JsonResult($"{user.UserName}, save to session");
+                return new JsonResult(user.UserName);
             }
-            else
-            {
-                Debug.Write("Login failed");
-                return new JsonResult(null);
-            }
+            return new JsonResult(null);
+        }
+
+        [HttpPost]
+        [Route("{username}/{password}")]
+        public JsonResult Post(string username, string password)
+        {
+            // ToDo: ->  look if user already exists
+            int salt = 12;
+            User user = null;
+            user.UserName = username;
+            user.UserPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+            // ToDo: ->  Insert with linq
+            HttpContext.Session.Set<User>(username, user);
+            return new JsonResult(user.UserName);
         }
 
         [HttpGet]
@@ -55,17 +56,13 @@ namespace WebApplication1
             return new JsonResult($"session clear");
         }
 
-        protected User GetUser(string username, string password)
+        protected User GetUserFromDB(string username, string password)
         {
             ErpSystemDbDataContext model = new ErpSystemDbDataContext();
             User user = model.Users.Where(user => user.UserName == username).FirstOrDefault();
             //if (BCrypt.Net.BCrypt.Verify(password, user.UserPassword))
             if(user.UserPassword == password)
-            {
-                Debug.WriteLine("Password korrekt");
                 return user;
-            }
-            Debug.WriteLine("Password inkorrekt");
             return null;
         }
     }
